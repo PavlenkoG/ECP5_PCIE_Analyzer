935441b0cfab36c01d51d568124ca214b7bb2065
Grigory PAVLENKO <Grigory.PAVLENKO@bachmann.info>
2021-08-24 07:26:58 +0200

    analyzer daily work

--- fpga_logic/src/analyzer.vhd
+++ fpga_logic/src/analyzer.vhd
@@ -18,6 +18,7 @@ architecture arch of analyzer is
         timestamp               : std_logic_vector (31 downto 0);
         byte_counter            : std_logic_vector (7 downto 0);
         data_reg                : std_logic_vector (31 downto 0);
+        data_ext_reg            : std_logic_vector (3 downto 0);
 
         seq_num                 : std_logic_vector (15 downto 0);
         tlp_len                 : std_logic_vector (9 downto 0);
@@ -28,17 +29,19 @@ architecture arch of analyzer is
         log_ena                 : std_logic;
         addr_pointer            : std_logic_vector (14 downto 0);
         addr_counter            : std_logic_vector (14 downto 0);
-        addr_amount             : std_logic_vector (14 downto 0);
+        data_amount             : std_logic_vector (14 downto 0);
         trigger                 : std_logic;
 
         packet_type             : t_packet_type;
         tlp_type                : t_tlp_type;
+        dllp_type               : t_dllp_type;
     end record t_reg;
 
     constant REG_T_INIT : t_reg := (
         timestamp               => (others => '0'),
         byte_counter            => (others => '0'),
         data_reg                => (others => '0'),
+        data_ext_reg            => (others => '0'),
 
         seq_num                 => (others => '0'),
         tlp_len                 => (others => '0'),
@@ -49,11 +52,12 @@ architecture arch of analyzer is
         log_ena                 => '0',
         addr_pointer            => (others => '0'),
         addr_counter            => (others => '0'),
-        addr_amount             => (others => '0'),
+        data_amount             => (others => '0'),
         trigger                 => '0',
 
         packet_type             => IDLE,
-        tlp_type                => MRD
+        tlp_type                => NO_PCK,
+        dllp_type               => NO_PCK
     );
 
     signal r, rin : t_reg;
@@ -62,6 +66,18 @@ begin
         variable v: t_reg;
     begin
         v := r;
+
+        if d.trigger_start = '1' then
+            v.log_ena := '1';
+        end if;
+        if d.trigger_stop = '1' or r.data_amount = X"7FFF" then
+            v.log_ena := '0';
+            v.timestamp := (others => '0');
+        end if;
+
+        if r.log_ena = '1' then
+            v.timestamp := std_logic_vector (unsigned(r.timestamp) + 1);
+        end if;
         -- start TLP Packet
         if d.rx_k = '1' and d.data_in_scr = K_STP_SYM_27_7 then
             v.packet_type := TLP_PKT;
@@ -72,8 +88,11 @@ begin
             v.packet_type := DLLP_PKT;
             v.byte_counter := std_logic_vector(unsigned(r.byte_counter) + 1);
         end if;
+        -- end TLP/DLLP Packet
         if d.rx_k = '1'and d.data_in_scr = K_END_SYM_29_7 then
             v.packet_type := IDLE;
+            v.tlp_type := NO_PCK;
+            v.dllp_type := NO_PCK;
             v.byte_counter := (others => '0');
         end if;
 
@@ -133,7 +152,6 @@ begin
                     end case;
                 -- TC
                 when 4 =>
-
                 -- Attribute & Lenght
                 when 5 =>
                     v.tlp_len (9 downto 8) := d.data_in_scr(1 downto 0);
@@ -153,10 +171,54 @@ begin
                     v.dw := d.data_in_scr;
                 when others => 
             end case;
-            
+        end if;
+        if r.packet_type = DLLP_PKT then
+            case to_integer(unsigned(r.byte_counter)) is
+                when 1 => 
+                    case d.data_in_scr is
+                        when DLLP_TYPE_ACK =>
+                            v.dllp_type := ACK;
+                        when DLLP_TYPE_NAK =>
+                            v.dllp_type := NAK;
+                        when DLLP_TYPE_PM_L1 =>
+                            v.dllp_type := PM_L1;
+                        when DLLP_TYPE_PM_L23 =>
+                            v.dllp_type := PM_L23;
+                        when DLLP_TYPE_PM_ASR1 =>
+                            v.dllp_type := PM_ASR1;
+                        when DLLP_TYPE_REQ_ACK =>
+                            v.dllp_type := REQ_ACK;
+                        when DLLP_TYPE_VEN_SP =>
+                            v.dllp_type := VEN_SP;
+                        when DLLP_TYPE_FC1P =>
+                            v.dllp_type := FC1P;
+                        when DLLP_TYPE_FC1NP =>
+                            v.dllp_type := FC1NP;
+                        when DLLP_TYPE_FC1CPL =>
+                            v.dllp_type := FC1CPL;
+                        when DLLP_TYPE_FC2P =>
+                            v.dllp_type := FC2P;
+                        when DLLP_TYPE_FC2NP =>
+                            v.dllp_type := FC2NP;
+                        when DLLP_TYPE_FC2CPL =>
+                            v.dllp_type := FC2CPL;
+                        when DLLP_TYPE_FCP =>
+                            v.dllp_type := FCP;
+                        when DLLP_TYPE_FCNP =>
+                            v.dllp_type := FCNP;
+                        when DLLP_TYPE_FCCPL =>
+                            v.dllp_type := FCCPL;
+                        when others =>
+                    end case;
+                when others => 
+            end case;
         end if;
 
-        
+        -- logger logic
+        if r.packet_type /= IDLE then
+            if r.log_ena = '1' then
+            end if;
+        end if;
 
 
         rin <= v;
--- fpga_logic/src/analyzer_pkg.vhd
+++ fpga_logic/src/analyzer_pkg.vhd
@@ -4,16 +4,16 @@ use ieee.std_logic_1164.all;
 package analyzer_pkg is
     constant MEM_LEN            : integer := 16;
 
-    constant K_COM_SYM_28_5     : std_logic_vector(7 downto 0) := X"BC"; -- comma; used for lane and link initialization and management
-    constant K_STP_SYM_27_7     : std_logic_vector(7 downto 0) := X"FB"; -- start TLP; Marks the start of a transaction layer packet
-    constant K_SDP_SYM_28_2     : std_logic_vector(7 downto 0) := X"5C"; -- start DLLP; marks the start of a data link layer packet
-    constant K_END_SYM_29_7     : std_logic_vector(7 downto 0) := X"FD"; -- end; marks the end of a TLP packet or DLLP packet
-    constant K_EDB_SYM_30_7     : std_logic_vector(7 downto 0) := X"FE"; -- EnD Bad; marks the end of nullified TLP
     constant K_PAD_SYM_23_7     : std_logic_vector(7 downto 0) := X"F7"; -- PAD; used in framing and link width and lane ordering negotiations
+    constant K_STP_SYM_27_7     : std_logic_vector(7 downto 0) := X"FB"; -- start TLP; Marks the start of a transaction layer packet
     constant K_PAD_SKP_28_0     : std_logic_vector(7 downto 0) := X"1C"; -- skip; used for compensating for different bit rates 
     constant K_PAD_FTS_28_1     : std_logic_vector(7 downto 0) := X"30"; -- Fast Training Sequence; Used within a ordered set to exit from L0s to L0
+    constant K_SDP_SYM_28_2     : std_logic_vector(7 downto 0) := X"5C"; -- start DLLP; marks the start of a data link layer packet
     constant K_PAD_IDL_28_3     : std_logic_vector(7 downto 0) := X"7C"; -- Idle; used in the electrical idle ordered set
+    constant K_COM_SYM_28_5     : std_logic_vector(7 downto 0) := X"BC"; -- comma; used for lane and link initialization and management
     constant K_PAD_EIE_28_7     : std_logic_vector(7 downto 0) := X"FC"; -- Electrical Idle Exit; Reserved in 2.5 GT/s
+    constant K_END_SYM_29_7     : std_logic_vector(7 downto 0) := X"FD"; -- end; marks the end of a TLP packet or DLLP packet
+    constant K_EDB_SYM_30_7     : std_logic_vector(7 downto 0) := X"FE"; -- EnD Bad; marks the end of nullified TLP
 
     -- TLP Packet Types
     --                                                               FMT TYPE
@@ -57,9 +57,9 @@ package analyzer_pkg is
     constant DLLP_TYPE_FCCPL   : std_logic_vector (7 downto 0) := B"1010_0000"; -- UpdateFC-Cpl
 
     type t_packet_type is (DLLP_PKT, TLP_PKT, ORDR_ST, IDLE);
-    type t_tlp_type is (MRD, MRDLK, MWR, IORD, IOWR, CFGRD0, CFGWR0, CFGRD1, CFGWR1, TCFGRD, TCFGWR, MSG, MSGD, CPL, CPLD, CPLLK, CPLDLK);
-    type t_dllp_type is (ACK, NAK, PM_L1, PM_L23, PM_ASR1, REQ_ACK, VEN_SP, FC1P, FC1NP, FC1CPL, FC2P, FC2NP, FC2CPL, FCP, FCNP, FCCPL);
-    type t_order_set_type is (TS1, TS2, SKIP, FTS, EIDLE);
+    type t_tlp_type is (NO_PCK, MRD, MRDLK, MWR, IORD, IOWR, CFGRD0, CFGWR0, CFGRD1, CFGWR1, TCFGRD, TCFGWR, MSG, MSGD, CPL, CPLD, CPLLK, CPLDLK);
+    type t_dllp_type is (NO_PCK, ACK, NAK, PM_L1, PM_L23, PM_ASR1, REQ_ACK, VEN_SP, FC1P, FC1NP, FC1CPL, FC2P, FC2NP, FC2CPL, FCP, FCNP, FCCPL);
+    type t_order_set_type is (NO_PCK, TS1, TS2, SKIP, FTS, EIDLE);
 
     type t_trigger_type is record
         packet_type_en      : std_logic;
@@ -78,13 +78,21 @@ package analyzer_pkg is
         addr_match          : std_logic_vector (31 downto 0);
     end record;
 
+    type t_filter_in is record
+        tlp_save            : std_logic;
+        dllp_save           : std_logic;
+        order_set_save      : std_logic;
+    end record;
+
     type t_analyzer_in is record
         data_in_unscr       : std_logic_vector (7 downto 0);
         data_in_scr         : std_logic_vector (7 downto 0);
         rx_k                : std_logic;
 
         trigger_start       : std_logic;
+        trigger_stop        : std_logic;
         trigger_set         : t_trigger_type;
+        filter_in           : t_filter_in;
     end record;
 
     type t_analyzer_out is record
